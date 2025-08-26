2c71f983ae30fc1c68622ae9be96033f4fe94eb7
552020 <stefanolombardo@posteo.de>
2025-08-26 23:42:00 +0200

    feat: add whoami function to backend canister

--- src/backend/src/lib.rs
+++ src/backend/src/lib.rs
@@ -1,4 +1,14 @@
+use candid::Principal;
+
 #[ic_cdk::query]
 fn greet(name: String) -> String {
     format!("Hello, {}!", name)
 }
+
+#[ic_cdk::query]
+fn whoami() -> Principal {
+    ic_cdk::api::msg_caller()
+}
+
+// Export the interface for the smart contract.
+ic_cdk::export_candid!();
